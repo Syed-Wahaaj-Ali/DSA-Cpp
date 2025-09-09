b6ba7e5d25596cc0f54bcc0c38d5456cd9886481
Syed Wahaaj Ali <syedwahaajali2006@gmail.com>
2025-09-09 05:05:35 +0500

    Update reverse_array.cpp
    Updated code to take user input

--- Arrays/reverse_array.cpp
+++ Arrays/reverse_array.cpp
@@ -1,29 +1,23 @@
 #include <iostream>
 using namespace std;
 
-// Function to reverse the array
-void reverseArray(int arr[], int n) {
-    int start = 0, end = n - 1;
-
-    // swap first and last, then move towards center
-    while (start < end) {
-        swap(arr[start], arr[end]);
-        start++;
-        end--;
-    }
-}
-
 int main() {
-    int arr[] = {1, 2, 3, 4, 5};
-    int n = 5;
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
 
-    cout << "Original array: ";
+    int arr[n];
+    cout << "Enter " << n << " elements: ";
     for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
+        cin >> arr[i];
     }
-    cout << endl;
 
-    reverseArray(arr, n);
+    // Reverse the array
+    for (int i = 0, j = n - 1; i < j; i++, j--) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 
     cout << "Reversed array: ";
     for (int i = 0; i < n; i++) {
