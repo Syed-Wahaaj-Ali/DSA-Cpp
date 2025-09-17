6d9d6b566cb9c0dfb1ae8386b97dd4ad21bcc9ab
Syed Wahaaj Ali <syedwahaajali2006@gmail.com>
2025-09-17 16:52:18 +0500

    Create count_nodes.cpp
    Added program to count the total number of nodes in a doubly linked list with user input.

---
+++ LinkedList/Doubly_LinkedList/count_nodes.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* prev;
+    Node* next;
+};
+
+// Function to insert node at end
+void insertAtEnd(Node*& head, Node*& tail, int value) {
+    Node* newNode = new Node;
+    newNode->data = value;
+    newNode->prev = tail;
+    newNode->next = nullptr;
+
+    if (tail != nullptr) {
+        tail->next = newNode;
+    } else {
+        head = newNode; // list was empty
+    }
+
+    tail = newNode;
+}
+
+void printList(Node* head) {
+    Node* temp = head;
+    while (temp != nullptr) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+// Function to count nodes
+int countNodes(Node* head) {
+    int count = 0;
+    Node* temp = head;
+    while (temp != nullptr) {
+        count++;
+        temp = temp->next;
+    }
+    return count;
+}
+
+int main() {
+    Node* head = nullptr;
+    Node* tail = nullptr;
+    int n, value;
+
+    cout << "Enter number of nodes: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cout << "Enter value for node " << i + 1 << ": ";
+        cin >> value;
+        insertAtEnd(head, tail, value);
+    }
+
+    cout << "Current list: ";
+    printList(head);
+    cout << "Total nodes in the list: " << countNodes(head) << endl;
+    return 0;
+}
